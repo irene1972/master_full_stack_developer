b9d925217d629894e79c1b4c2e43fbd6a12c6ba0
Irene Olmos Gomez <ireneog_72@hotmail.es>
2026-02-12 22:12:18 +0100

    update blog

--- proyecto6_act5_angular/blogNoticias/src/app/components/blog/blog.ts
+++ proyecto6_act5_angular/blogNoticias/src/app/components/blog/blog.ts
@@ -12,7 +12,6 @@ import { Posts } from '../../services/posts';
 })
 export class Blog {
   
-  //misPosts:IPost[]=[];
   misPosts:IPost[]=[];
 
   postsService=inject(Posts);
@@ -29,39 +28,18 @@ export class Blog {
 
   ngOnInit(){
     let response=this.postsService.getAll();
-    console.log(response);
     this.misPosts=response;
     
   }
 
-  cargarDatos($event:any):void{
-    let formularioDom=$event.target;
-    let insertar;
+  cargarDatos():void{
+    
     if(this.miFormulario.valid){
-      insertar=this.postsService.insert(this.miFormulario.value);
+      this.postsService.insert(this.miFormulario.value);
     }
-    console.log($event.target);
-    formularioDom.querySelector('#titulo').value='';
-    formularioDom.querySelector('#imagen').value='';
-    formularioDom.querySelector('#noticia').value='';
-    formularioDom.querySelector('#fecha').value='';
-
-    let div=document.createElement('DIV');
-
-    if(insertar){
-      div.textContent=insertar;
-      div.classList.add('exito');
-    }else{
-      div.textContent='Ha habido un error';
-      div.classList.add('error');
-    }
-
-    formularioDom.appendChild(div);
+    
     this.miFormulario.reset();
 
-    setTimeout(()=>{
-      div.remove();
-    },3000);
   }
 
 }
